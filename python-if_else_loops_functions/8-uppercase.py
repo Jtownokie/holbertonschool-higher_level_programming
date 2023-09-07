a6077ff9eed4d14c38020263592bd6a435fdd0ca
Jtownokie <6673@holbertonstudents.com>
2023-09-07 13:17:49 -0700

    Formatting fix

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@
 def uppercase(str):
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
-            print("{}".format(chr(ord(char) - 32)))
+            print("{}".format(chr(ord(char) - 32)), end="")
         else:
-            print("{}".format(char))
+            print("{}".format(char), end="")
