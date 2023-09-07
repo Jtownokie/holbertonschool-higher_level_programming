8f1af96832875092e5b3f6b275c1f816143e1f7d
Jtownokie <6673@holbertonstudents.com>
2023-09-06 22:50:23 -0700

    print alphabet task

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -9,8 +9,8 @@ if number >= 0:
     elif (number % 10) == 0:
         print(f"Last digit of {number} is {number % 10} and is 0")
     elif (number % 10) < 6 and (number % 10) != 0:
-        print(f"Last digit of {number} is {number % 10}\
-          and is less than 6 and not 0")
+        print(f"Last digit of {number} is {number % 10} \
+and is less than 6 and not 0")
 else:
     print(f"Last digit of {number} is -{abs(number) % 10} \
 and is less than 6 and not 0")
