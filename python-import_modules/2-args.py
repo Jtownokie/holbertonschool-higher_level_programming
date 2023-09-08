5b12e51255033770b46c9f00339d3b8924a2836a
Jtownokie <6673@holbertonstudents.com>
2023-09-07 19:28:48 -0700

    Test fix on argv length

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,12 +3,12 @@
 from sys import argv as argv
 
 if __name__ == "__main__":
-    argc = len(argv)
+    argc = len(argv) - 1
     if argc == 0:
         print("{} arguments.".format(argc))
     elif argc == 1:
         print("{} argument:\n1: {}".format(argc, argv[1]))
     elif argc > 1:
         print("{} arguments:")
-        for i in range(1, argc):
+        for i in range(1, argc + 1):
             print("{}: {}".format(i, argv[i]))
