dd71dc4d731be3d32b93a3610b79b1fc589e1cfc
Jtownokie <6673@holbertonstudents.com>
2023-09-07 19:26:26 -0700

    argv task completed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+from sys import argv as argv
+
+if __name__ == "__main__":
+    argc = len(argv)
+    if argc == 0:
+        print("{} arguments.".format(argc))
+    elif argc == 1:
+        print("{} argument:\n1: {}".format(argc, argv[1]))
+    elif argc > 1:
+        print("{} arguments:")
+        for i in range(1, argc):
+            print("{}: {}".format(i, argv[i]))
