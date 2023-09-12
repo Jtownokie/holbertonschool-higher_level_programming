db49084c7378d8572748b6044ba2d4f6b6041350
Jtownokie <6673@holbertonstudents.com>
2023-09-12 08:46:09 -0700

    Changed list to iterate by item instead of index

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,10 +2,10 @@
 
 def divisible_by_2(my_list=[]):
     if my_list:
-        result_list = my_list.copy()
-        for i in range(0, len(my_list)):
-            if my_list[i] % 2 == 0:
-                result_list[i] = True
+        result_list = []
+        for item in my_list:
+            if item % 2 == 0:
+                result_list.append(True)
             else:
-                result_list[i] = False
+                result_list.append(False)
     return result_list
