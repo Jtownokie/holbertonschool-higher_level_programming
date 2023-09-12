9c8751585102142ec48598fb9bcccb08590f5696
Jtownokie <6673@holbertonstudents.com>
2023-09-11 23:19:37 -0700

    Simplified code

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    current_max = 0
-    for i in range(0, len(my_list) - 1):
-        if my_list[i] > current_max:
-            current_max = my_list[i]
-        else:
-            continue
-    
-    return current_max
+    if my_list:
+        my_list.sort()
+        length = (len(my_list)) - 1
+        return my_list[length]
+    else:
+        return
