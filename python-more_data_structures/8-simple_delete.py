a6c963fff6eb3a9568bd5e4ee268dd3bf3ab75b0
Jtownokie <6673@holbertonstudents.com>
2023-09-12 16:18:07 -0700

    Function that deletes a key/value pair in a dictionary

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    if key == None:
+        return a_dictionary
+    else:
+        del a_dictionary[key]
+        return a_dictionary
