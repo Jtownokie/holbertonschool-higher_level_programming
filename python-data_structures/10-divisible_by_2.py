3cbf14d514483c9afb55d155db352258d3268534
Jtownokie <6673@holbertonstudents.com>
2023-09-11 23:31:17 -0700

    Function that determines if list elements are divisible by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    if my_list:
+        result_list = my_list.copy()
+        for i in range(0, len(my_list)):
+            if my_list[i] % 2 == 0:
+                result_list[i] = True
+            elif my_list[i] % 2 != 0:
+                result_list[i] = False
+    return result_list
