e08e9c4163c40872b9a387f543a633f8fcba2bf2
Jtownokie <6673@holbertonstudents.com>
2023-09-12 12:36:10 -0700

    Fixed new_matrix declaratin

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[]]
+    new_matrix = []
     for iter in range(len(matrix)):
         new_matrix.append(list(map(lambda item: item ** 2, matrix[iter])))
     return new_matrix
