d23f1f943cd825bddabd1894bf1b854060f0e77b
Jtownokie <6673@holbertonstudents.com>
2023-09-11 23:32:13 -0700

    changed else clause

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -6,6 +6,6 @@ def divisible_by_2(my_list=[]):
         for i in range(0, len(my_list)):
             if my_list[i] % 2 == 0:
                 result_list[i] = True
-            elif my_list[i] % 2 != 0:
+            else:
                 result_list[i] = False
     return result_list
