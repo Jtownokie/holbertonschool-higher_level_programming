11405358db04c5ef0acfdf5a5bba25d42ca149b5
Jtownokie <6673@holbertonstudents.com>
2023-09-11 21:34:48 -0700

    Added any() edge case check

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) != 0:
+    if any(matrix):
         for i in range(0, len(matrix)):
             for j in range(0, len(matrix)):
                 if j < len(matrix) - 1:
                     print("{:d}".format(matrix[i][j]), end=' ')
                 else:
                     print("{:d}".format(matrix[i][j]))
+    else:
+        print("")
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
