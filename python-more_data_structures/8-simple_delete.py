79f3f768eff53e0d2fb2c835e63b84d7c714a8a6
Jtownokie <6673@holbertonstudents.com>
2023-09-12 16:20:28 -0700

    Fixed edge case

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if key == "":
+    if key in a_dictionary:
+        del a_dictionary[key]
         return a_dictionary
     else:
-        del a_dictionary[key]
         return a_dictionary
