518a92ce908014640be7d0e06c7025119180f97c
Jtownokie <6673@holbertonstudents.com>
2023-09-11 18:32:02 -0700

    added edge case for empty list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for item in my_list:
-        print("{}".format(item))
+    if type(my_list) == list:
+        for item in my_list:
+            print("{}".format(item))
