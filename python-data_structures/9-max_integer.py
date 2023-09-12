ee5e255476b7e4349d378d43fac75c2e57e4c211
Jtownokie <6673@holbertonstudents.com>
2023-09-11 23:18:20 -0700

    Function that returns max integer in a list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    current_max = 0
+    for i in range(0, len(my_list) - 1):
+        if my_list[i] > current_max:
+            current_max = my_list[i]
+        else:
+            continue
+    
+    return current_max
