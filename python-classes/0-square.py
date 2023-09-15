fa04b0c6942a7b9fed62ef29c369bd91047a333b
Jtownokie <6673@holbertonstudents.com>
2023-09-15 07:43:43 -0700

    Fixed spacing to conform with pycodestyle

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" This Module contains an empty class for the Python - Classes Project Task 0"""
+""" This is an empty class for the Python - Classes Project Task 0"""
+
 
 class square:
     """ This is an empty class for the Python - Classes Project Task 0"""
