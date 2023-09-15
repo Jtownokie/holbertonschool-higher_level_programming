9a5e5334008b6e4ac2f9000627b6a15467cf3e30
Jtownokie <6673@holbertonstudents.com>
2023-09-15 11:42:14 -0700

    Added instantiation with __init__

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This is a class that defines a square for Classes Project Task 1"""
+
+
+class Square:
+    """ This is a class that defines a square for Classes Project Task 1"""
+    def __init__(self, size):
+        self.__size = size
