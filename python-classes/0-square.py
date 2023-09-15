356d2ffaf155a58ff694e5697e4d586cf1dafc88
Jtownokie <6673@holbertonstudents.com>
2023-09-15 07:42:02 -0700

    Added docstring to module

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" This Module contains an empty class for the Python - Classes Project Task 0"""
 
 class square:
     """ This is an empty class for the Python - Classes Project Task 0"""
