264d810f11850e7d95ce113123e2f1de470a3f35
Jtownokie <6673@holbertonstudents.com>
2023-09-18 08:41:05 -0700

    Corrected edge case check and implemented integer casting

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" This module contains one function 'add_integer' that returns a sum """
 
 def add_integer(a, b=98):
-    if type(a) is not int or type(b) is not int:
-        if type(a) is not float:
-            raise TypeError('a must be an integer')
-        elif type(b) is not float:
-            raise TypeError('b must be an integer')
-    return a + b
+    """ This function adds two integers and returns the result """
+    if type(a) is not int or type(a) is not float:
+        raise TypeError('a must be an integer')
+    elif type(b) is not int or type(b) is not float:
+        raise TypeError('b must be an integer')
+    else:
+        return int(a) + int(b)
