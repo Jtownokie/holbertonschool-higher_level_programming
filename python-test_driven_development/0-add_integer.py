2e2e7cdc4f355107ba168d9c506e48847ec26e98
Jtownokie <6673@holbertonstudents.com>
2023-09-18 09:03:49 -0700

    Added Test cases for infinite float or none passed as argument

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ This module contains one function 'add_integer' that returns a sum """
 
+
 def add_integer(a, b=98):
     """ This function adds two integers and returns the result """
-    if type(a) is not int and type(a) is not float:
+    if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError('a must be an integer')
     elif type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
