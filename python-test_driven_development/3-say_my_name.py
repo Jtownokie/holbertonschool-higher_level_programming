db3eb6f9fe593b3b180a70480deddc195e03ef7e
Jtownokie <6673@holbertonstudents.com>
2023-09-18 12:31:26 -0700

    Function that prints a name, added edge cases for non string inputs

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" This module contains a function that prints a first and last name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ This Function prints 'My name is <first_name> <last_name>' """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    elif type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+    else:
+        print(f"My name is {first_name} {last_name}")
