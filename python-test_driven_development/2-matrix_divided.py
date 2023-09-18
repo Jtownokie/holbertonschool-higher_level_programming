d52870650a7921bb627395b767298d6202b63e5f
Jtownokie <6673@holbertonstudents.com>
2023-09-18 11:32:43 -0700

    Wrote base function to divide elements of matrix by number, added edge case for matrix type

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" This Module contains a function that divides matrix elements """
+
+
+def matrix_divided(matrix, div):
+    """ This Function divides every element of a matrix by a number """
+    new_matrix = []
+
+    if all([isinstance(elem, list) for elem in matrix]) is False:
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    elif all([all([isinstance(elem, (int, float)) for elem in list])
+              for list in matrix]) is False:
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    else:
+        new_matrix = [[elem / div for elem in list] for list in matrix]
+        return new_matrix
