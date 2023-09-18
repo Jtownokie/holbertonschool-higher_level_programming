c00dcbc2471e7e750dea8a8d704cb4026de934b6
Jtownokie <6673@holbertonstudents.com>
2023-09-18 11:45:37 -0700

    Added Edge Case test for matrix of lists of different length

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,6 +5,8 @@
 def matrix_divided(matrix, div):
     """ This Function divides every element of a matrix by a number """
     new_matrix = []
+    if type(matrix[0]) is list:
+        length = len(matrix[0])
 
     if all([isinstance(elem, list) for elem in matrix]) is False:
         raise TypeError('matrix must be a matrix (list of lists) '
@@ -13,6 +15,8 @@ def matrix_divided(matrix, div):
               for list in matrix]) is False:
         raise TypeError('matrix must be a matrix (list of lists) '
                         'of integers/floats')
+    elif all([len(row) == length for row in matrix]) is False:
+        raise TypeError('Each row of the matrix must have the same size')
     else:
         new_matrix = [[elem / div for elem in list] for list in matrix]
         return new_matrix
