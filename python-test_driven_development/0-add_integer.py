7371e35e1e56a60832058d99336b85cd5aaa324c
Jtownokie <6673@holbertonstudents.com>
2023-09-18 09:11:16 -0700

    Added edge case for float('NaN') passed as argument

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,7 +4,9 @@
 
 def add_integer(a, b=98):
     """ This function adds two integers and returns the result """
-    if a is None or (type(a) is not int and type(a) is not float):
+    if a != a or b != b:
+        raise ValueError('cannot convert float NaN to integer')
+    elif a is None or (type(a) is not int and type(a) is not float):
         raise TypeError('a must be an integer')
     elif type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
