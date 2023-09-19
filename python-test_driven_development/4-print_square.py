213642afe69ee3f95e82f0fdfde0ed8a5b9101bc
Jtownokie <6673@holbertonstudents.com>
2023-09-18 17:43:14 -0700

    Function that prints a square with edge cases implemented

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" This Module contains a function that prints a square """
+
+
+def print_square(size):
+    """ This function prints a square of size 'size' """
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+    elif size == 0:
+        print("")
+        return
+    
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
