a7a8e906b86e5e1d916f47d69c471e855facbe98
Jtownokie <6673@holbertonstudents.com>
2023-09-18 18:13:35 -0700

    Wrote function, implemented edge case for non str input and missing argument

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" This Module contains a function that adds newlines in a string """
+
+
+def text_indentation(text):
+    """ This Function adds two newlines after every ., ?, and : """
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    text1 = text.replace('. ', '.\n\n')
+    text2 = text1.replace('? ', '?\n\n')
+    final_text = text2.replace(': ', ':\n\n')
+
+    print(final_text)
