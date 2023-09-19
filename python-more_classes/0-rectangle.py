f3baac176d6ed949b4b05a64a9dd44b8f75623d2
Jtownokie <6673@holbertonstudents.com>
2023-09-18 22:13:01 -0700

    Created More_Classes directory and completed task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" This is a module defining a Class called Rectangle """
+
+class Rectangle:
+    """ This is a Class called Rectangle """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is the README for the More Classes Project for Holberton School
