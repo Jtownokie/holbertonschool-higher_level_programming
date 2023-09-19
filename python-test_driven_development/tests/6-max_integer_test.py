afdfa24a4c8929d63922e6d53e41426f9ef3239f
Jtownokie <6673@holbertonstudents.com>
2023-09-18 20:05:03 -0700

    Implemented test units for empty argument, exception raising, and negative numbers

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,3 +7,14 @@ class TestMaxInteger(unittest.TestCase):
     def test_list(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+        self.assertEqual(max_integer([-4, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([-5, -4, -2, -3]), -2)
+
+    def test_empty(self):
+        self.assertIsNone(max_integer(None))
+        self.assertIsNone(max_integer([]))
+
+    def test_errors(self):
+        self.assertRaises(TypeError, max_integer, [1, 2, 'hi'])
+        
