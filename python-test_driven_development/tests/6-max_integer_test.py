aab2b45ce2d9b9e2992f04f84ed02defeb3326d7
Jtownokie <6673@holbertonstudents.com>
2023-09-18 19:58:06 -0700

    Testing initial asserts

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,8 +5,5 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_list(self):
-        self.assertEqual([1, 2, 3, 4], 4)
-        self.assertEqual([1, 3, 4, 2], 4)
-    
-    def test_errors(self):
-        self.assertRaises(TypeError, max_integer, 'hi')
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
