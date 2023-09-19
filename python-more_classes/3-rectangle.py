d3161bb43aa9e078a999b1f2b4fb08a06d78c893
Jtownokie <6673@holbertonstudents.com>
2023-09-18 22:53:28 -0700

    added docstring

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
         self.height = height
 
     def __str__(self):
+        """ ___str___ method for Rectangle Class """
         string_rep = ""
         for i in range(self.__height):
             for j in range(self.__width):
