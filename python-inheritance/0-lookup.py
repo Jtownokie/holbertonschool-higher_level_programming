e236aa13e56dea0717965e408e002feb5eacae97
Jtownokie <6673@holbertonstudents.com>
2023-09-24 12:09:15 -0700

    Set up Directory and completed lookup function task

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This Module contains the 'lookup' function """
+
+
+def lookup(obj):
+    """ This Function uses 'dir' to return a list of attributes """
+    return (dir(obj))
---
+++ python-inheritance/README.md
@@ -0,0 +1,1 @@
+This is the README for the Inheritance Project for Holberton School
