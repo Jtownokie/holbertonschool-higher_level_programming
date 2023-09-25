f05df393d98d5e23da81c602c4aa55e1cd25ea9b
Jtownokie <6673@holbertonstudents.com>
2023-09-25 08:33:13 -0700

    Implemeted integer validator method and tested for Type and Value errors

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" This module contains a Class 'BaseGeometry' """
+
+
+class BaseGeometry:
+    """ This is the BaseGeomety Class """
+    def area(self):
+        """ This is a placeholder method """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """ This method validates an integer value """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        else:
+            return
