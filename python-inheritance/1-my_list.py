1034c23f957d6badae50a4d3834ffc7aeeaa058a
Jtownokie <6673@holbertonstudents.com>
2023-09-24 20:01:45 -0700

    Added MyList Class and print_sorted method

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" This Module contains the MyList Class """
+
+
+class MyList(list):
+    """ This Class is derived from the 'list' type and can sort a list """
+    def print_sorted(self):
+        """ This method prints a sorted list """
+        print(sorted(self))
