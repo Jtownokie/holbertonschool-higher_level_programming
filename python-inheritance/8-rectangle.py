ad2b862e99f9270ed9841ce405522ca4afedf518
Jtownokie <6673@holbertonstudents.com>
2023-09-25 11:36:30 -0700

    Implemented Rectangle subclass with __init__ method

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" This Module contains a Class 'Rectangle'
+    that is a subclass of BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ This Class is a subclass of BaseGeometry to define a Rectangle """
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
