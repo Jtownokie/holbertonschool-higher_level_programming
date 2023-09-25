bd3a6b2a86d021e10180613900b71ead81cc7038
Jtownokie <6673@holbertonstudents.com>
2023-09-25 07:54:41 -0700

    Function that checks if object is subclass of a class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" This module contains a function
+    that checks if an object is a subclass of a class """
+
+
+def inherits_from(obj, a_class):
+    """ This Function returns True if obj is a subclass of a_class """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
