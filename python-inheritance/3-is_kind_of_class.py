cd4f03c51716dcc3761a76acfb7df88d6c31fcfa
Jtownokie <6673@holbertonstudents.com>
2023-09-25 07:40:31 -0700

    Function that checks if object is an instance of a class

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" This module contains a function
+    that checks if an object is an instance of a class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ This Function returns True if obj is an instance of a_class """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
