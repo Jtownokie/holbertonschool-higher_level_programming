4755f844b2f34fe58838682447242c555d0c0103
Jtownokie <6673@holbertonstudents.com>
2023-09-25 08:00:37 -0700

    Implemented area method with exception raise

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" This module contains a Class 'BaseGeometry' """
+
+
+class BaseGeometry:
+    """ This is the BaseGeomety Class """
+    def area(self):
+        raise Exception('area() is not implemented')
