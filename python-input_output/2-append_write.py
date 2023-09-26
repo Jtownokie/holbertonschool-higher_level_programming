47fb9a7f98c43e127ea5fdf4e25b0cbab063344a
Jtownokie <6673@holbertonstudents.com>
2023-09-26 15:26:45 -0700

    Implemented append_write function

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" This Module contains a function that appends to a file """
+
+
+def append_write(filename="", text=""):
+    """ This function opens and appends to a file """
+    with open(filename, 'r+', encoding="utf-8") as f:
+        charsprinted = f.write(text)
+
+    return charsprinted
