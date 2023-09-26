0e2a66e96f94e289b69a3337b6903614ca7646e3
Jtownokie <6673@holbertonstudents.com>
2023-09-26 15:28:24 -0700

    corrected open() 'mode' parameter

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,7 +4,7 @@
 
 def append_write(filename="", text=""):
     """ This function opens and appends to a file """
-    with open(filename, 'r+', encoding="utf-8") as f:
+    with open(filename, 'a+', encoding="utf-8") as f:
         charsprinted = f.write(text)
 
     return charsprinted
