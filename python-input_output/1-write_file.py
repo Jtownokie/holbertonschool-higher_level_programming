e85d402f95a2d4e8e51946fc52201a83838e98bc
Jtownokie <6673@holbertonstudents.com>
2023-09-26 15:15:18 -0700

    Function that opens and writes to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" This Module contains a function that writes to a file """
+
+
+def write_file(filename="", text=""):
+    """ This function opens and writes to a file """
+    with open(filename, 'w+', encoding="utf-8") as f:
+        f.write(text)
