a09720a180d5d2cdd782bb280b1ac913f35839c1
Jtownokie <6673@holbertonstudents.com>
2023-09-26 20:23:45 -0700

    Function that returns the __dict__ of a class instance

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" This module contains a function that
+    prepares a class instance for JSON serialization"""
+
+
+def class_to_json(obj):
+    """ This module returns the __dict__ of a class instance """
+    return obj.__dict__
