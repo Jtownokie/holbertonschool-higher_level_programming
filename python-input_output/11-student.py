309f6c8cc55a581401752a2ab2353934e7ec4308
Jtownokie <6673@holbertonstudents.com>
2023-09-26 21:09:52 -0700

    Implemented reload_from_json method

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+""" This module contains a class 'Student' """
+
+
+class Student:
+    """ This Class defines a 'Student' with name and age """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ This method returns the attributes of a Class instance
+            referred to by 'attrs' """
+        new_dict = {}
+
+        if type(attrs) is not list or attrs is None:
+            return self.__dict__
+        else:
+            for element in attrs:
+                if type(element) is not str:
+                    return self.__dict__
+                if element in self.__dict__.keys():
+                    new_dict[element] = self.__dict__[element]
+
+        return new_dict
+
+    def reload_from_json(self, json):
+        """ This Method replaces all attributes of an instance """
+        for key in json.keys():
+            if key in self.__dict__.keys():
+                self.__dict__[key] = json[key]
