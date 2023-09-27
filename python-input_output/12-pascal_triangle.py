b2bc04b0a56a7767604cc6bba420ab2c002f9600
Jtownokie <6673@holbertonstudents.com>
2023-09-26 21:32:30 -0700

    Pascal's Triangle Function completed

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" This module contains a technical interview response that returns a
+    representation of Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """ This function returns a list of lists of integers representing
+        the Pascal's triangle of 'n' """
+    if n <= 0:
+        return []
+    else:
+        pascal_list = []
+
+        for i in range(1, n + 1):
+            current_elem = 1
+            row_list = []
+            for j in range(1, i + 1):
+                row_list.append(current_elem)
+                current_elem = current_elem * (i - j) // j
+            pascal_list.append(row_list)
+    return pascal_list
