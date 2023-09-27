f8cd2770b1a6c9d020604ede0c56d18ab6b5a8db
Jtownokie <6673@holbertonstudents.com>
2023-09-26 19:16:55 -0700

    Implemented function that loads a JSON to an object

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" This Module contains a function that loads
+    a JSON representation into an object """
+import json
+
+
+def load_from_json_file(filename):
+    """ This function converts 'my_obj' into a
+        JSON representation and writes it to a file """
+    with open(filename, 'r', encoding="utf-8") as f:
+        json.load(f)
