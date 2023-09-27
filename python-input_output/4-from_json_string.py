f40e4139ebfd707d461f3a0b00e52673804b90e0
Jtownokie <6673@holbertonstudents.com>
2023-09-26 19:08:58 -0700

    Implemented JSON to object function

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" This Module contains a function that returns
+    a decoded JSON representation of an object """
+import json
+
+
+def from_json_string(my_str):
+    """ This function converts 'my_str' into an object """
+    return json.loads(my_str)
