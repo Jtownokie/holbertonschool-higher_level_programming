51cfb5f18e38a963301db048f3db3e567f774343
Jtownokie <6673@holbertonstudents.com>
2023-09-29 10:37:03 -0700

    Added staticmethod decorator

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ This Method converts list_dictionaries into a JSON string """
         if list_dictionaries is None or list_dictionaries == []:
