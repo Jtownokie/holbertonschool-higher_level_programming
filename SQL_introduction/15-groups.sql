9f8adab7dea9ee7452e84bc1c385a0d1ef58a8b1
root <root@C-PC>
2023-10-20 22:51:49 -0500

    Updated grouping script

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- Lists number of records with the same score
 SELECT score, COUNT(*) AS number
-FROM second_table GROUP BY score HAVING number > 1
+FROM second_table GROUP BY score
 ORDER BY number DESC;
