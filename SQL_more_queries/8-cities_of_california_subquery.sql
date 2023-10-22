008668eecd18e8d880d41d41556b4bd7d96d7a77
root <root@C-PC>
2023-10-21 21:03:06 -0500

    Corrected subquery script

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- Subqueries two tables
 SELECT id, name FROM cities
 WHERE state_id = 
-    (SELECT state_id
+    (SELECT id
      FROM states
      WHERE name = 'California')
 ORDER BY cities.id ASC;
