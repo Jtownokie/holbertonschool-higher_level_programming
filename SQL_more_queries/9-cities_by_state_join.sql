aea4d179c60b9e4ce7f44bc85f0e20c0afe70582
root <root@C-PC>
2023-10-22 00:33:13 -0500

    Corrected join script

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- Joins two tables
 SELECT cities.id, cities.name, states.name
-    FROM cities
-        NATURAL JOIN states ORDER BY cities.id ASC;
+FROM cities INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
