d461f8217b0423895e36314c50c689c9a9db8be3
root <root@C-PC>
2023-10-23 12:36:40 -0500

    SQL Querty safe from injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" Contains MySQLdb script that queries a table """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+
+    state_input = sys.argv[4]
+
+    cursor.execute("SELECT * FROM states WHERE name = '%s' "
+                   "ORDER BY id ASC", (state_input, ))
+    states = cursor.fetchall()
+
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+
+    cursor.close()
+    db.close()
