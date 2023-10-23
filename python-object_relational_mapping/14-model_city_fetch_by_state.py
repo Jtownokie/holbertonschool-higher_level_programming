17b94d09662f82631d9219d10f68b064434dfb3a
root <root@C-PC>
2023-10-23 18:06:51 -0500

    SQLAlchemy join script

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" SQLAlchemy Script that queries cities/states """
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities_states = session.query(City).join(State).order_by(City.id).all()
+
+    for city, state in cities_states:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" This Module contains a Class definition for a SQLAlchemy Class """
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """ This class connects a SQL database to the ORM """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
