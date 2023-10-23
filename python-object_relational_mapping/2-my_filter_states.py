93b518f46a5fac539259afad72b101f169b22c67
root <root@C-PC>
2023-10-23 00:18:16 -0500

    Testing syntax correction

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name = {} ORDER BY id ASC"
-                   .format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name = {} ORDER BY id ASC".format(sys.argv[4]))
     states = cursor.fetchall()
 
     for state in states:
