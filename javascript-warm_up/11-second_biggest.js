98522ff920a07294e239bf42a29938eab4bee2d2
Jtownokie <6673@holbertonstudents.com>
2023-11-25 15:08:05 -0600

    Completed Second Largest Integer Script

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+import { argv } from 'node:process';
+
+if (argv.length > 3) {
+  const argInts = [];
+  for (let i = 2; i < argv.length; i++) {
+    argInts.push(argv[i]);
+  }
+
+  argInts.sort(function (a, b) { return a - b; });
+  const secondBiggest = argInts[argInts.length - 2];
+
+  console.log(secondBiggest);
+} else {
+  console.log('0');
+}
