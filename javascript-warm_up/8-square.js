09ea3ecc6a85798d213fb761fae8c1966032f885
Jtownokie <6673@holbertonstudents.com>
2023-11-25 14:18:16 -0600

    Completed Square printing Script

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+import { argv } from 'node:process';
+
+const squareSize = Number(argv[2]);
+
+if (squareSize) {
+  for (let i = 0; i < squareSize; i++) {
+    let row = '';
+    for (let j = 0; j < squareSize; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
