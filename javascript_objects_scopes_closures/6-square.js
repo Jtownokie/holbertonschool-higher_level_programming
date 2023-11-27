271364ff29172f52bc714636dfb766645b4ff7f5
Jtownokie <6673@holbertonstudents.com>
2023-11-27 14:27:20 -0600

    Added charPrint method

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+module.exports = class Square extends Square {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += `${c}`;
+        }
+        console.log(row);
+      }
+    } else {
+      this.print();
+    }
+  }
+};
