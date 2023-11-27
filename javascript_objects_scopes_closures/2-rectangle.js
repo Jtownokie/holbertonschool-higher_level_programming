1bef580dc33109e51a0e3feac6f55a1eb302ed1b
Jtownokie <6673@holbertonstudents.com>
2023-11-27 13:12:11 -0600

    Constructor validation

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w < 1 || h < 1) {
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
