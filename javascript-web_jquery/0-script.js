2ecd97ec5589767296fc4f1df69e635e56fb0094
Jtownokie <6673@holbertonstudents.com>
2023-12-01 09:33:30 -0600

    Completed basic JavaScript element selector script

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,3 @@
+const headerTag = document.querySelector('header');
+
+headerTag.style.color = '#ff0000';
---
+++ javascript-web_jquery/README.md
@@ -0,0 +1,1 @@
+This is the README for the JavaScript - Web JQuery Project for Holberton School
